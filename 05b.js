aa7ba3684fcfee66967f6cb32a0d17f13652562b
Tin Crnkovic <tinc2k@gmail.com>
2018-12-11 00:56:23 +0100

    Indent fix.

--- 05b.js
+++ 05b.js
@@ -15,29 +15,29 @@ for (let i = 0; i < alpha.length; i++) {
     letter = alpha[i];
   }
 }
- console.log(shortest, letter);
+console.log(shortest, letter);
 console.timeEnd();
 
 function add(polymer, unit) {
- if (polymer.length === 0) {
+  if (polymer.length === 0) {
    polymer.push(unit);
    return;
- }
- let lastUnit = polymer[polymer.length - 1];
- if (unit.toLowerCase() !== unit && unit.toLowerCase() === lastUnit ||
-     unit.toUpperCase() !== unit && unit.toUpperCase() === lastUnit) {
-       polymer.pop();
- } else {
-   polymer.push(unit);
- }
+  }
+  let lastUnit = polymer[polymer.length - 1];
+  if (unit.toLowerCase() !== unit && unit.toLowerCase() === lastUnit ||
+      unit.toUpperCase() !== unit && unit.toUpperCase() === lastUnit) {
+    polymer.pop();
+  } else {
+    polymer.push(unit);
+  }
 }
 
 function react(polymer) {
- let newPolymer = [];
- for (let i = 0; i < polymer.length; i++) {
-   add(newPolymer, polymer[i]);
- }
- return newPolymer;
+  let newPolymer = [];
+  for (let i = 0; i < polymer.length; i++) {
+    add(newPolymer, polymer[i]);
+  }
+  return newPolymer;
 }
 function remove(polymer, unit) {
   return polymer.split(unit).join('').split(unit.toUpperCase()).join('');
