bf89534fafd50d6cfc027f10e3fd16b7543dfd98
Tin Crnkovic <tinc2k@gmail.com>
2018-12-11 00:53:34 +0100

    Fix 05b, which I've previously broken. Attempt console.time() / console.timeEnd().

--- 05b.js
+++ 05b.js
@@ -1,54 +1,44 @@
 // https://adventofcode.com/2018/day/5
-
 const fs = require('fs');
-
 let start = (new Date).getTime();
 let polymer = fs.readFileSync('05input.txt', 'utf8');
-let lower = 'abcdefghijklmnopqrstuvwxyz';
-
+let alpha = 'abcdefghijklmnopqrstuvwxyz';
 let letter = null;
 let shortest = polymer.length;
-for (let i = 0; i < shortest.length; i++) {
-  let shorter = remove(polymer, lower[i]);
+
+console.time();
+for (let i = 0; i < alpha.length; i++) {
+  let shorter = remove(polymer, alpha[i]);
   let reacted = react(shorter);
-  if (reacted.length < length) {
-    length = reacted.length;
-    letter = shortest[i];
+  if (reacted.length < shortest) {
+    shortest = reacted.length;
+    letter = alpha[i];
   }
 }
-
-console.log({letter, length});
-console.log(`done in ${(new Date).getTime() - start}ms.`);
+ console.log(shortest, letter);
+console.timeEnd();
 
 function add(polymer, unit) {
-  if (polymer.length === 0) {
-    polymer.push(unit);
-    return;
-  }
-  let lastUnit = polymer[polymer.length - 1];
-  if (unit.toLowerCase() !== unit && unit.toLowerCase() === lastUnit ||
-      unit.toUpperCase() !== unit && unit.toUpperCase() === lastUnit) {
-    polymer.pop();
-  } else {
-    polymer.push(unit);
-  }
+ if (polymer.length === 0) {
+   polymer.push(unit);
+   return;
+ }
+ let lastUnit = polymer[polymer.length - 1];
+ if (unit.toLowerCase() !== unit && unit.toLowerCase() === lastUnit ||
+     unit.toUpperCase() !== unit && unit.toUpperCase() === lastUnit) {
+       polymer.pop();
+ } else {
+   polymer.push(unit);
+ }
 }
 
 function react(polymer) {
-  let newPolymer = [];
-  for (let i = 0; i < polymer.length; i++) {
-    add(newPolymer, polymer[i]);
-  }
-  return newPolymer;
+ let newPolymer = [];
+ for (let i = 0; i < polymer.length; i++) {
+   add(newPolymer, polymer[i]);
+ }
+ return newPolymer;
 }
-
 function remove(polymer, unit) {
   return polymer.split(unit).join('').split(unit.toUpperCase()).join('');
-  // let newPolymer = [];
-  // for (let i = 0; i < polymer.length; i++) {
-  //   if (polymer[i] !== unit.toLowerCase() && polymer[i] !== unit.toUpperCase()) {
-  //     newPolymer.push(polymer[i]);
-  //   }
-  // }
-  // return newPolymer.join('');
 }
